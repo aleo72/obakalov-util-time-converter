9f5f3ca78a533260ef4f3c20c5e0ac2f89f268fc
Bakalov Oleksii <aleo.bakalov@gmail.com>
2016-05-11 08:58:05 +0300

    add new test

--- src/main/scala/obakalov/util/time/Converter.scala
+++ src/main/scala/obakalov/util/time/Converter.scala
@@ -1,5 +1,7 @@
 package obakalov.util.time
 
+import java.time.ZoneId
+
 import scala.language.implicitConversions
 
 /**
@@ -15,11 +17,8 @@ class Converter {
     * @param jodaDateTime instance
     * @return object of [[java.time.LocalDateTime]]
     */
-  def jodaDateTimeToJavaLocalDateTime(jodaDateTime: org.joda.time.DateTime): java.time.LocalDateTime = {
-    java.time.LocalDateTime.ofInstant(
-      java.time.Instant.ofEpochMilli(jodaDateTime.toInstant.getMillis),
-      java.time.ZoneId.systemDefault()
-    )
+  def jodaLocalDateTimeToJavaLocalDateTime(jodaDateTime: org.joda.time.LocalDateTime): java.time.LocalDateTime = {
+    java.time.LocalDateTime.ofInstant(jodaDateTime.toDate.toInstant, ZoneId.systemDefault())
   }
 
   /**
@@ -28,12 +27,32 @@ class Converter {
     * @param localDateTime instance of [[java.time.LocalDateTime]]
     * @return object of [[org.joda.time.DateTime]]
     */
-  def javaLocalDateTimeToJodaDateTime(localDateTime: java.time.LocalDateTime): org.joda.time.DateTime = {
-    new org.joda.time.DateTime(
+  def javaLocalDateTimeToJodaDateTime(localDateTime: java.time.LocalDateTime): org.joda.time.LocalDateTime = {
+    new org.joda.time.LocalDateTime(
       localDateTime.atZone(
         java.time.ZoneId.systemDefault()
       ).toInstant.toEpochMilli
     )
+  }
+
+  /**
+    * Converting [[java.time.LocalDate]] to [[org.joda.time.LocalDate]]
+    *
+    * @param date instance of [[java.time.LocalDate]]
+    * @return object of [[org.joda.time.LocalDate]]
+    */
+  def javaLocalDateToJodaLocalDate(date: java.time.LocalDate): org.joda.time.LocalDate = {
+    new org.joda.time.LocalDate(date.getYear, date.getMonthValue, date.getDayOfMonth)
+  }
+
+  /**
+    * Converting [[org.joda.time.LocalDate]] to [[java.time.LocalDate]]
+    *
+    * @param jodaDate instance of [[org.joda.time.LocalDate]]
+    * @return object of [[java.time.LocalDate]]
+    */
+  def jodaLocalDateToJavaLocalDate(jodaDate: org.joda.time.LocalDate): java.time.LocalDate = {
+    java.time.LocalDate.ofYearDay(jodaDate.getYear, jodaDate.getDayOfYear)
   }
 }
 
@@ -41,9 +60,13 @@ object Converter extends Converter {
 
   object Implicits {
 
-    implicit val jodaDateTimeToJavaLocalDateTimeImplicits: org.joda.time.DateTime => java.time.LocalDateTime = Converter.jodaDateTimeToJavaLocalDateTime
+    implicit def jodaLocalDateTimeToJavaLocalDateTimeImplicits(d: org.joda.time.LocalDateTime): java.time.LocalDateTime = Converter.jodaLocalDateTimeToJavaLocalDateTime(d)
 
-    implicit val localDateTimeToJodaDateTime: java.time.LocalDateTime => org.joda.time.DateTime = Converter.javaLocalDateTimeToJodaDateTime
+    implicit def javaLocalDateTimeToJodaDateTimeImplicits(d: java.time.LocalDateTime): org.joda.time.LocalDateTime = Converter.javaLocalDateTimeToJodaDateTime(d)
+
+    implicit def javaLocalDateToJodaLocalDateImplicits(d: java.time.LocalDate): org.joda.time.LocalDate = Converter.javaLocalDateToJodaLocalDate(d)
+
+    implicit def jodaLocalDateToJavaLocalDateImplicits(d: org.joda.time.LocalDate): java.time.LocalDate = Converter.jodaLocalDateToJavaLocalDate(d)
   }
 
 }
---
+++ src/test/scala/obakalov/util/time/test/ConverterSpec.scala
@@ -0,0 +1,74 @@
+package obakalov.util.time.test
+
+import org.scalatest.{FlatSpec, Matchers}
+
+/**
+  * Created by abakalov on 10.05.16.
+  *
+  * @author abakalov
+  */
+class ConverterSpec extends FlatSpec with Matchers {
+  val converter = new obakalov.util.time.Converter
+
+  import Data._
+
+  "Class Converter" should "JavaLocalDateTime to JodaLocalDateTime" in {
+    javaLocalDateTime shouldEqual converter.jodaLocalDateTimeToJavaLocalDateTime(jodaDateTime)
+  }
+
+  it should "JodaLocalDateTime to JavaLocalDateTime" in {
+    jodaDateTime shouldEqual converter.javaLocalDateTimeToJodaDateTime(javaLocalDateTime)
+  }
+
+  it should "JavaLocalDate to JodaLocalDate" in {
+    javaLocalDate shouldEqual converter.jodaLocalDateToJavaLocalDate(jodaLocalDate)
+  }
+
+  it should "JodaLocalDate to JavaLocalDate" in {
+    jodaLocalDate shouldEqual converter.javaLocalDateToJodaLocalDate(javaLocalDate)
+  }
+  
+  "Implicits Converter" should "JavaLocalDateTime to JodaLocalDateTime" in {
+    import obakalov.util.time.Converter.Implicits.jodaLocalDateTimeToJavaLocalDateTimeImplicits
+    val _jodaDateTime: java.time.LocalDateTime = jodaDateTime
+    javaLocalDateTime shouldEqual _jodaDateTime
+  }
+
+  it should "JodaLocalDateTime to JavaLocalDateTime" in {
+    import obakalov.util.time.Converter.Implicits.javaLocalDateTimeToJodaDateTimeImplicits
+    val _javaDateTime: org.joda.time.LocalDateTime = javaLocalDateTime
+    jodaDateTime shouldEqual _javaDateTime
+
+  }
+
+  it should "JavaLocalDate to JodaLocalDate" in {
+    import obakalov.util.time.Converter.Implicits.javaLocalDateToJodaLocalDateImplicits
+    val _javaLocalDate: org.joda.time.LocalDate = javaLocalDate
+    jodaLocalDate shouldEqual _javaLocalDate
+  }
+
+
+  it should "JodaLocalDate to JavaLocalDate" in {
+    import obakalov.util.time.Converter.Implicits.jodaLocalDateToJavaLocalDateImplicits
+    val _jodaLocalDate: java.time.LocalDate = jodaLocalDate
+    javaLocalDate shouldEqual _jodaLocalDate
+  }
+
+  object Data {
+    val year = 2000
+    val month = 1
+    val dayOfMonth = 10
+    val hour = 10
+    val minute = 28
+    val seconds = 18
+    val nanoOfSecond = 0
+
+    val javaLocalDateTime = java.time.LocalDateTime.of(year, month, dayOfMonth, hour, minute, seconds, nanoOfSecond)
+    val jodaDateTime = new org.joda.time.LocalDateTime(year, month, dayOfMonth, hour, minute, seconds, 0)
+
+    val javaLocalDate = java.time.LocalDate.of(year, month, dayOfMonth)
+    val jodaLocalDate = new org.joda.time.LocalDate(year, month, dayOfMonth)
+
+  }
+
+}
